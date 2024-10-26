792a43763f007fb984bacc2a7f7a841b868debe7
Hoàng Phi <121500912+Hoang-Phi-1026@users.noreply.github.com>
2024-10-26 18:50:44 +0700

    phi

--- layout/navqlchuoi.php
+++ layout/navqlchuoi.php
@@ -4,7 +4,7 @@
         <div class="dropdown">
             <a href="#">Quản lý <i class="fas fa-caret-down"></i></a>
             <div class="dropdown-content">
-            <a href="/cuahangthucannhanh/views/QLNV/index.php">Quản lý Nhân Viên <i class="fas fa-user"></i></a>
+            <a href="/chuoithucannhanh/views/QLNV/index.php">Quản lý Nhân Viên <i class="fas fa-user"></i></a>
                 <a href="index.php?page=qlnlchuoicuahang">Quản lý nguyên liệu <i class="fas fa-warehouse"></i></a>
                 <a href="index.php?page=quanlydonhang">quản lý đơn hàng<i class="fas fa-box"></i></a>
                 <a href="#">Quản lý .......... <i class="fas fa-file-invoice"></i></a>
@@ -16,6 +16,15 @@
                 <a href="#">Thống kê........ <i class="fas fa-chart-line"></i></a>
                 <a href="#">Thống kê nguyên liệu <i class="fas fa-seedling"></i>
                 <a href="#">Thống kê........<i class="fas fa-users"></i></a>
+            </div>
+        </div>
+
+        <div class="dropdown">
+            <a href="#">Nhân sự <i class="fas fa-caret-down"></i></a>
+            <div class="dropdown-content">
+                <a href="/chuoithucannhanh/views/ChamCong/index.php">Chấm công <i class="fas fa-chart-line"></i></a>
+                <a href="#">Xếp lịch <i class="fas fa-seedling"></i>
+                <a href="#">Tính lương<i class="fas fa-users"></i></a>
             </div>
         </div>
 
