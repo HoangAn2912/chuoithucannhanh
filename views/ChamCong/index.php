7e188c9f050085341f4d578b3040298d708e2f15
Hoàng Phi <121500912+Hoang-Phi-1026@users.noreply.github.com>
2024-10-29 23:08:03 +0700

    up

--- views/ChamCong/index.php
+++ views/ChamCong/index.php
@@ -21,7 +21,7 @@
             <h2>Chấm Công Nhân Viên</h2>  
         </div>  
 
-        <div class="cc-search-bar">  
+        <div class="cc-search-bar">  t 
             <form method="POST">  
                 <input class="cc-input" type="text" name="search" placeholder="Nhập nhân viên cần tìm..." />  
                 <button class="cc-submit" type="submit"><i class="fas fa-search"></i> Tìm</button>  
