2ee4049b7316494a58f228f8d2521c4fff39668c
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2024-11-06 23:53:05 +0700

    1

--- views/dangnhap/xuly.php
+++ views/dangnhap/xuly.php
@@ -7,6 +7,7 @@ if(isset($_POST["login"])) {
     $taikhoan = $nguoidung->getNguoiDungByAccount ($email, $password);
     if($taikhoan){
         $_SESSION["dangnhap"] = $taikhoan[0]['mand'];
+        $_SESSION["mach"] = $taikhoan[0]['mach'];
         echo '<script>alert("Đăng nhập thành công");</script>';
         header("Location:index.php?page=trangchu");
     }
